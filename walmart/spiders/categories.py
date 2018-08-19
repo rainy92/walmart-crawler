ad9f4298442f7d450189bdcfe212d0f4b3b3cb90
rainy92 <rainy92@gmail.com>
2018-08-19 10:23:19 +0530

    categories spider is working

--- walmart/spiders/categories.py
+++ walmart/spiders/categories.py
@@ -1,10 +1,21 @@
 import scrapy
+from scrapy.selector import Selector
 
 
 class CategoriesSpider(scrapy.Spider):
     name = 'categories'
-    allowed_domains = ['www.walmart.com']
-    start_urls = ['http://www.walmart.com/']
+    start_urls = ['https://www.walmart.com/sitemap_category.xml']
 
     def parse(self, response):
-        pass
+    	response = Selector(text=response.body)
+
+        urls = response.xpath('//url')
+
+        for url in urls:
+        	location = url.xpath('./loc/text()').extract_first()
+        	last_modified = url.xpath('./lastmod/text()').extract_first()
+
+        	yield {
+        		'url': location,
+        		'last_modified': last_modified,
+        	}
